42cec92ee9ba2535a0e725d03f37b67c84008e4b
FaziletKosure <faziletkosure1@gmail.com>
2020-11-09 23:19:45 +0100

    add

--- daily_challenge/day4.js
+++ daily_challenge/day4.js
@@ -3,14 +3,12 @@
 function three_numbers(x, y, z) {
     if (x == y && y == z) {
       return 30;
-    }
-  
-    if (x == y || y == z || z == x) {
+    }else if(x == y || y == z || z == x) {
       return 40;
     }
   
     return 20;
   }
-  console.log(three_numbers(8, 8, 8));
+  console.log(three_numbers(6, 6, 6));
   console.log(three_numbers(8, 8, 18));
   console.log(three_numbers(8, 7, 18));
